82ea6ba6e4eb6c7bf6e2823b343b94039fe4d727
Papudim <thiago_soares_mago@hotmail.com>
2020-08-02 18:26:07 -0300

    Aula 4 Final

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -28,7 +28,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'https://thiago-flix.herokuapp.com/categorias';
+      const URL = 'http://localhost:8080/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
